db593dda491a4c69fc9455bede87b03c27107c5f
Ravi Shankar <ravishankar@Ravis-MacBook-Pro.local>
2024-07-22 16:30:33 +0530

    new change

--- schema/transaction-schema.js
+++ schema/transaction-schema.js
@@ -6,9 +6,9 @@ const TransactionSchema = new mongoose.Schema(
     {
 
         paymentId: { type: String, required: true},
-        ordeerId: { type: String, required: true},
+        orderId: { type: String, required: true},
         userId: { type: String, required: true},
-        tokenAmount: { type: String, required: true},
+        amount: { type: String, required: true},
 
     },
 
