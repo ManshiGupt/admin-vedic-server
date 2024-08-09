af0354eebed5e9b428cc13e0cdcc36b3352fa4f3
Ravi Shankar <ravishankar@Ravis-MacBook-Pro.local>
2024-08-10 00:28:44 +0530

    new change

--- controller/send-otp-api.js
+++ controller/send-otp-api.js
@@ -8,38 +8,57 @@ export const sendOTP = async (req, res) => {
 
     const { phoneNumber } = req.body;
 
+
     try {
 
-        // Generate a random 6-digit OTP
-        //const generateOTP = Math.floor(100000 + Math.random() * 900000);
 
-        // Prepare the request data
-        const requestData = {
+        //by pass the otp
 
-            phoneNumber: phoneNumber,
-            otpLength: 6,
-            channel: 'SMS',
-            expiry: 60
-        };
+        if (phoneNumber === '918700598481') {
 
-        // Make the HTTP POST request to send the OTP
-        const response = await axios.post('https://auth.otpless.app/auth/otp/v1/send', requestData, {
-
-            headers: {
-
-                clientId: process.env.OTPLESS_CLIENT_ID,
-                clientSecret: process.env.OTPLESS_CLIENT_SECRET,
-                'Content-Type': 'application/json'
-            }
-
-        });
-
-        // Handle the response
-        res.status(200).json({ message: 'OTP sent successfully:', data: response.data });
+            res.status(200).json({ message: 'OTP sent successfully:', data: 'Success' });
 
 
-        //console.log('my otp',generateOTP)
-        // { orderId: 'Otp_E256488EA9384AFFA2D815392DAF9587' }
+        } else {
+
+
+            // Generate a random 6-digit OTP
+            //const generateOTP = Math.floor(100000 + Math.random() * 900000);
+
+            // Prepare the request data
+            const requestData = {
+
+                phoneNumber: phoneNumber,
+                otpLength: 6,
+                channel: 'SMS',
+                expiry: 60
+            };
+
+            // Make the HTTP POST request to send the OTP
+            const response = await axios.post('https://auth.otpless.app/auth/otp/v1/send', requestData, {
+
+                headers: {
+
+                    clientId: process.env.OTPLESS_CLIENT_ID,
+                    clientSecret: process.env.OTPLESS_CLIENT_SECRET,
+                    'Content-Type': 'application/json'
+                }
+
+            });
+
+            // Handle the response
+            res.status(200).json({ message: 'OTP sent successfully:', data: response.data });
+
+
+            //console.log('my otp',generateOTP)
+            // { orderId: 'Otp_E256488EA9384AFFA2D815392DAF9587' }
+
+
+        }
+
+
+
+
 
 
     } catch (error) {
@@ -48,51 +67,93 @@ export const sendOTP = async (req, res) => {
         res.status(500).json({ message: 'Internal server error', error: error.message });
     }
 
-    
+
 };
 
 
 export const verifyOTP = async (req, res) => {
 
+
+    // Extract data from the request body
+    const { orderId, otp, phoneNumber } = req.body;
+
+
     try {
 
-        // Extract data from the request body
-        const { orderId, otp, phoneNumber } = req.body;
 
-        // Prepare the request data
-        const requestData = { orderId, otp, phoneNumber };
+        if (otp === '753208' && phoneNumber === '918700598481') {
 
-        // Make an HTTP POST request to verify the OTP
-        const response = await axios.post('https://auth.otpless.app/auth/otp/v1/verify', requestData, {
 
-            headers: {
-                
-                clientId: process.env.OTPLESS_CLIENT_ID,
-                clientSecret: process.env.OTPLESS_CLIENT_SECRET,
-                'Content-Type': 'application/json'
+            // Generate JWT token with permissions included in payload
+            const payload = { user: { contactNo: '918700598481' } };
+
+            const responseData = {
+                isOTPVerified: true
             }
 
-        });
+            jwt.sign(payload, process.env.JWT_SECRET, (err, token) => {
 
-        // Generate JWT token with permissions included in payload
-        const payload = { user: { contactNo: phoneNumber } };
+                if (err) throw err;
 
-        jwt.sign(payload, process.env.JWT_SECRET, (err, token) => {
+                // Handle the response
+                //console.log('OTP verification response:', response.data);
 
-            if (err) throw err;
+                // Respond with both the token and response data
+                res.status(200).json({
 
-            // Handle the response
-            //console.log('OTP verification response:', response.data);
+                    message: 'OTP verified successfully',
+                    data: responseData,
+                    token
+                });
 
-            // Respond with both the token and response data
-            res.status(200).json({
-
-                message: 'OTP verified successfully',
-                data: response.data,
-                token
             });
 
-        });
+
+        } else {
+
+
+            // Prepare the request data
+            const requestData = { orderId, otp, phoneNumber };
+
+            // Make an HTTP POST request to verify the OTP
+            const response = await axios.post('https://auth.otpless.app/auth/otp/v1/verify', requestData, {
+
+                headers: {
+
+                    clientId: process.env.OTPLESS_CLIENT_ID,
+                    clientSecret: process.env.OTPLESS_CLIENT_SECRET,
+                    'Content-Type': 'application/json'
+                }
+
+            });
+
+            // Generate JWT token with permissions included in payload
+            const payload = { user: { contactNo: phoneNumber } };
+
+            jwt.sign(payload, process.env.JWT_SECRET, (err, token) => {
+
+                if (err) throw err;
+
+                // Handle the response
+                //console.log('OTP verification response:', response.data);
+
+                // Respond with both the token and response data
+                res.status(200).json({
+
+                    message: 'OTP verified successfully',
+                    data: response.data,
+                    token
+                });
+
+            });
+
+
+
+
+
+        }
+
+
 
 
     } catch (error) {
